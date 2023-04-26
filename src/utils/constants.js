59be0b58fe6b720cd48a31f9c946bf19b45887c9
NadaMuhammad28 <nadamuhammad28@gmail.com>
2023-04-26 08:35:47 +0200

    constants and data

---
+++ src/utils/constants.js
@@ -0,0 +1,88 @@
+import {
+  AiOutlineGithub,
+  AiOutlineFilePdf,
+  AiOutlineLinkedin,
+} from "react-icons/ai";
+export const socialLinks = [
+  {
+    id: 1,
+    nsme: "github",
+    icon: AiOutlineGithub,
+    url: "https://github.com/NadaMuhammad28",
+  },
+  {
+    id: 2,
+    nsme: "linkedin",
+    icon: AiOutlineLinkedin,
+    url: "https://www.linkedin.com/in/nada-omran-5536251b8/",
+  },
+  {
+    id: 3,
+    nsme: "resume",
+    icon: AiOutlineFilePdf,
+    url: "https://drive.google.com/file/d/1YsKX-jzFSTePlAFXYjQHuWbXAvA8091d/view?usp=share_link",
+  },
+];
+
+export const skills = [
+  {
+    id: 1,
+    title: "HTML",
+    src: "/html.831b0a8a.svg",
+  },
+  {
+    id: 2,
+    title: "CSS",
+    src: "/css.9a2d55c8.svg",
+  },
+  {
+    id: 3,
+    title: "Bootstrap",
+    src: "/bootstrap.a071d875.svg",
+  },
+  {
+    id: 4,
+    title: "Tailwind",
+    src: "/tailwind.87eec164.svg",
+  },
+  {
+    id: 5,
+    title: "JavaScript",
+    src: "/javascript.7ae6c4c0.svg",
+  },
+  {
+    id: 6,
+    title: "ReactJS",
+    src: "/react.d0e9dc0b.svg",
+  },
+  {
+    id: 7,
+    title: "RTK",
+    src: "/redux.svg",
+  },
+  {
+    id: 8,
+    title: "TypeScript",
+    src: "/typescript.c4db509c.svg",
+  },
+  {
+    id: 9,
+    title: "NextJS",
+    src: "/next.afac9015.svg",
+  },
+  {
+    id: 10,
+    title: "NodeJS",
+    src: "/node.ce6a695c.svg",
+  },
+  {
+    id: 11,
+    title: "Git / GitHub ",
+    src: "/git.273a5c87.svg",
+  },
+  // {
+  //   id: 12,
+  //   title: "NodeJS",
+  //   src: "/node.ce6a695c.svg",
+  // },
+];
